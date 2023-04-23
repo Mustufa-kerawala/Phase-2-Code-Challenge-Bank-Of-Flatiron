1dfc5f495b117e6e2b65bb6e99a866e823fda43f
Mustufa-kerawala <mustufakerawala@outlook.com>
2023-04-23 13:19:36 +0300

    Cleaned up dev env and added transaction class which renders a table for each transaction as a component and imported it to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import './index.css';
+import Transactions from './Transactions';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <header className="App-header">
+       
+       <h3>Transactions</h3>
+       
+        
+      </header>
+      <Transactions />
     </div>
   );
 }
---
+++ src/Transactions.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+
+class Transactions extends Component {
+    // Creating a constructor for the component
+    constructor(props) {
+        super(props);
+        this.state = {
+            transactions: []
+        };
+    }
+   
+    
+    // Creating a getData function to fetch the data from the API
+    getData() {
+        fetch("http://localhost:3000/transactions")
+        .then(response => response.json())
+        .then( data => {
+            this.setState({ transactions: data.data });
+        });
+    }
+    
+    // Creating a render function for rendering each transaction
+    render() {
+        return (
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th> Date</th>
+                            <th> Description</th>
+                            <th>Category</th>
+                            <th>Amount</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.transactions.map(transaction => (
+                            <tr key={transaction.id}>
+                                <td>{transaction.date}</td>
+                                <td>{transaction.description}</td>
+                                <td>{transaction.category}</td>
+                                <td>{transaction.amount}</td>
+                                
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
+
+}
+
+
+export default Transactions;
