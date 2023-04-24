3df207fb193bc949c64fb4df34def39075fccd6c
Mustufa-kerawala <mustufakerawala@outlook.com>
2023-04-24 08:58:56 +0300

    New Approach using Functions and Closures

---
+++ src/TransactionList.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Transaction from "./Transaction";
+
+
+function TransactionList({ transaction }) {
+    const transactionList = transaction.map((transaction) => (
+        <Transaction
+            key={transaction.id}
+            date={transaction.date}
+            description={transaction.description}
+            category={transaction.category}
+            amount={transaction.amount}
+        />));
+
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Description</th>
+                    <th>Category</th>
+                    <th>Amount</th>
+                </tr>
+            </thead>
+            <tbody>{transactionList}</tbody>
+        </table>
+    );
+}
+
+export default TransactionList;
---
+++ src/TransactionsContainer.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import TransactionList from "./TransactionList";
+
+
+function TransactionsContainer() {
+    const [transactions, setTransactions] = useState([]);
+    const [query, setQuery] = useState("");
+
+    
+    function handleSearch(e) {
+        setQuery(e.target.value)
+      }
+    
+    
+    useEffect(() => {
+        fetch(" http://localhost:3000/transactions" + query)
+            .then((response) => response.json())
+            .then((data) => setTransactions(data));
+    }, [query]);
+
+    return (
+        <>
+            <input type="text" placeholder="Search..." onChange={handleSearch} />
+            <TransactionList transaction={transactions} />
+            
+        </>
+    );
+
+
+}
+
+export default Transactions;
